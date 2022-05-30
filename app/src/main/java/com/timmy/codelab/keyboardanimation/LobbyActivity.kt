3a82db9171736dfeaafee58a7fec17d2e732cc6b
Timmy Chen <timmy80713@gmail.com>
2022-05-30 21:42:23 +0800

    feat: create WindowInsetExtensions.

--- app/src/main/java/com/timmy/codelab/keyboardanimation/LobbyActivity.kt
+++ app/src/main/java/com/timmy/codelab/keyboardanimation/LobbyActivity.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.core.view.*
 import com.timmy.codelab.keyboardanimation.arch.ViewBindingActivity
 import com.timmy.codelab.keyboardanimation.databinding.ActivityLobbyBinding
+import com.timmy.codelab.keyboardanimation.widget.doOnWindowInsetsChanged
 import com.timmy.codelab.keyboardanimation.widget.keyboard.KeyboardAnimationCompat
 
 class LobbyActivity : ViewBindingActivity<ActivityLobbyBinding>() {
@@ -20,7 +21,8 @@ class LobbyActivity : ViewBindingActivity<ActivityLobbyBinding>() {
 
     private fun setupWindow() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        setupWindow1()
+//        setupWindow1()
+        setupWindow2()
     }
 
     private fun setupWindow1() {
@@ -41,5 +43,24 @@ class LobbyActivity : ViewBindingActivity<ActivityLobbyBinding>() {
                 }
             }
         )
+    }
+
+    private fun setupWindow2() {
+        viewBinding.root.doOnWindowInsetsChanged { _, insets ->
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            viewBinding.root.updatePadding(
+                left = systemBarsInsets.left,
+                right = systemBarsInsets.right
+            )
+            insets
+        }
+        viewBinding.bottomSpace.doOnWindowInsetsChanged(true) { _, insets ->
+            val systemInsets =
+                insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.ime())
+            viewBinding.bottomSpace.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                updateMargins(bottom = systemInsets.bottom)
+            }
+            insets
+        }
     }
 }
